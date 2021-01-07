352bb74f8c6213d9aae730c442b95317ff66e127
Marcos Canales <mcanalesmayo@gmail.com>
2021-01-07 11:02:26 +0900

    Add @AllArgsConstructor

--- src/main/java/test/introspected/PostBean.java
+++ src/main/java/test/introspected/PostBean.java
@@ -3,12 +3,14 @@ package test.introspected;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.http.annotation.Header;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import static io.micronaut.http.HttpHeaders.CONTENT_TYPE;
 
 @Introspected
 @Data // Lombok getters and setters
+@AllArgsConstructor
 public class PostBean extends BaseBean {
 
     @Header(CONTENT_TYPE)
